62648b6074979770dd00b8d07ba68f4139887c90
Trustin Harris <trust@trustins-macbook-pro.local>
2018-01-31 18:51:58 -0500

    Initial Commit

---
+++ MovieFlix/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MovieFlix
+//
+//  Created by Trustin Harris on 1/31/18.
+//  Copyright © 2018 Trustin Harris. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
